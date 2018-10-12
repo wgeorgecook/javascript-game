6cb5d9427f567e390fb8fde0173e4b7b85887543
William George Cook <wgeorgecook@gmail.com>
2018-10-11 19:34:20 -0700

    Gulp watching for changes to js, html, and css files

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            basedir: "/"
+        }
+    })
+});
+
+gulp.task('watch', ['browserSync'], function (){
+    gulp.watch('*.js', browserSync.reload);
+    gulp.watch('index.html', browserSync.reload);
+    gulp.watch('style.css', browserSync.reload)
+})
