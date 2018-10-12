21af3a2de8941c9f45f76292a8b0a8970acf512c
William George Cook <wgeorgecook@gmail.com>
2018-10-11 20:54:54 -0700

    Paddle skeleton

--- app/paddle.js
+++ app/paddle.js
@@ -1,18 +1,18 @@
 // Paddle at the bottom of the screen
 
-class Paddle {
+export default class Paddle {
 
     constructor(gameHeight, gameWidth) {
         this.width = 150;
         this.height = 30;
         this.pos = {
-            x: (gameHeight/2),
+            x: (gameWidth / 2) - (this.width / 2),
             y: gameHeight - this.height - 10
         };
     }
 
     draw(ctx) {
-
+        ctx.fillRect(this.pos.x, this.pos.y, this.width, this.height)
     }
 
 }
