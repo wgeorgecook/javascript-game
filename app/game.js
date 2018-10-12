b243b03db8fcab2e746ca7f52aedc0008f2de18b
William George Cook <wgeorgecook@gmail.com>
2018-10-11 21:05:47 -0700

    Import and draw the paddle

--- app/game.js
+++ app/game.js
@@ -1,15 +1,15 @@
 // Game Engine
-import Paddle from './paddle.js'
+import Paddle from '/app/paddle.js'
 
 let canvas = document.querySelector('#gameScreen');
 let ctx = canvas.getContext('2d');
 
-const GAME_HEIGHT = 800;
-const GAME_WIDTH = 600;
+const GAME_HEIGHT = 600;
+const GAME_WIDTH = 800;
 
 
 // clear the screen on render
-ctx.clearRect(0, 0, GAME_HEIGHT, GAME_WIDTH);
+ctx.clearRect(0, 0, 800, 600);
 
 // Draw things
 ctx.fillStyle = '#00f';
