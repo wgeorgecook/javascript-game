cdeab149d0fdb43b73584baab9d25bf411a97633
William George Cook <wgeorgecook@gmail.com>
2018-10-11 20:25:24 -0700

    Setting the canvas width/height

--- app/game.js
+++ app/game.js
@@ -3,8 +3,11 @@
 let canvas = document.querySelector('#gameScreen');
 let ctx = canvas.getContext('2d');
 
+const GAME_HEIGHT = 800;
+const GAME_WIDTH = 600;
+
+
 // clear the screen on render
-ctx.clearRect(0, 0, 600, 600);
+ctx.clearRect(0, 0, GAME_HEIGHT, GAME_WIDTH);
 
 // Draw things
-ctx.fillRect(20, 20, 100, 100)
