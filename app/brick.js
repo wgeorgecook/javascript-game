44df947a9a121de71a22d53680eea0c1d0425613
William George Cook <wgeorgecook@gmail.com>
2018-10-13 08:25:58 -0700

    Stage brick to delete on collision

--- app/brick.js
+++ app/brick.js
@@ -1,3 +1,5 @@
+import { collisionDetection } from '/app/collisionDetection.js'
+
 export default class Brick {
     constructor(game, pos) {
         this.img = document.querySelector('#img_brick');
@@ -5,10 +7,14 @@ export default class Brick {
         this.pos = pos;
         this.width = 80;
         this.height = 24;
+        this.toDelete = false;
     }
 
     update(dt) {
-
+        if( collisionDetection(this.game.ball, this) ) {
+            this.game.ball.speed.y = - this.game.ball.speed.y
+            this.toDelete = true;
+        }
     }
 
     draw(ctx) {
