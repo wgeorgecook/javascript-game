022f4b135ec183a3dc59f7fb3e2191bf3476ba37
William George Cook <wgeorgecook@gmail.com>
2018-10-13 08:04:29 -0700

    Changed level design to be more symetrical

--- app/levels.js
+++ app/levels.js
@@ -6,7 +6,7 @@ export function buildLevel(game, level) {
     level.forEach( ( row, rowIndex ) => {
         row.forEach( ( brick, brickIndex ) => {
             if ( brick === 1 ) {
-                let pos = { x: 80 * brickIndex, y: 20 + 24 * rowIndex }
+                let pos = { x: 80 * brickIndex, y: 75 + 24 * rowIndex }
                 bricks.push( new Brick(game, pos) )
             }
         });
@@ -16,7 +16,7 @@ export function buildLevel(game, level) {
 }
 
 export const level1 = [
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
