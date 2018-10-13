f646586fe893509f50515e309d6005843552987f
William George Cook <wgeorgecook@gmail.com>
2018-10-12 17:50:12 -0700

    Passing paddle to inputhandler

--- app/game.js
+++ app/game.js
@@ -1,5 +1,6 @@
 // Game Engine
-import Paddle from '/app/paddle.js'
+import Paddle from '/app/paddle.js';
+import InputHandler from '/app/input.js';
 
 let canvas = document.querySelector('#gameScreen');
 let ctx = canvas.getContext('2d');
@@ -7,9 +8,12 @@ let ctx = canvas.getContext('2d');
 const GAME_HEIGHT = 600;
 const GAME_WIDTH = 800;
 
+
 let paddle = new Paddle(GAME_HEIGHT, GAME_WIDTH);
 
 paddle.draw(ctx)
+
+new InputHandler(paddle);
 
 let lastTime = 0;
 
@@ -18,7 +22,7 @@ function gameLoop(timeStamp) {
     lastTime = timeStamp;
     // clear the screen on render
     ctx.clearRect(0, 0, 800, 600);
-    paddle.update(dt);
+    // paddle.update(dt);
     paddle.draw(ctx);
     requestAnimationFrame(gameLoop);
 }
