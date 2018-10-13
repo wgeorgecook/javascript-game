d9d1c3e94fcf2eed96d42762fd8f63f78cf8895a
William George Cook <wgeorgecook@gmail.com>
2018-10-12 21:32:56 -0700

    Ball bounces off walls

--- app/ball.js
+++ app/ball.js
@@ -1,13 +1,34 @@
 export default class Ball {
-    constructor() {
+    constructor(gameHeight, gameWidth) {
+        this.gameHeight = gameHeight;
+        this.gameWidth = gameWidth;
         this.img = document.querySelector('#img_ball');
+        this.speed = {
+            x: 2,
+            y: 2
+        };
+        this.pos = {
+            x: 10,
+            y: 10
+        };
+        this.size = 16;
     }
 
     draw(ctx) {
-        ctx.drawImage(this.img, 10, 10)
+        ctx.drawImage(this.img, this.pos.x, this.pos.y, this.size, this.size)
     }
 
-    update() {
-        // to do
+    update(dt) {
+        this.pos.x += this.speed.x;
+        this.pos.y += this.speed.y;
+
+        // wall collisions
+        if (this.pos.x + this.size > this.gameWidth || this.pos.x < 0) {
+            this.speed.x = -this.speed.x
+        }
+
+        if (this.pos.y + this.size > this.gameHeight || this.pos.y < 0) {
+            this.speed.y = -this.speed.y
+        }
     }
 }
