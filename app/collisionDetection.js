e84f0409fb0f03ac50712dd7db55ad181b148c18
William George Cook <wgeorgecook@gmail.com>
2018-10-13 08:16:23 -0700

    Fixed positioning bug

--- app/collisionDetection.js
+++ app/collisionDetection.js
@@ -1,21 +1,13 @@
 
 export function collisionDetection(ball, gameObject) {
-        // wall collisions
-        if (ball.pos.x + ball.size > this.game.gameWidth || ball.pos.x < 0) {
-            ball.speed.x = -ball.speed.x
-        }
-
-        if (ball.pos.y + ball.size > this.game.gameHeight || ball.pos.y < 0) {
-            ball.speed.y = -ball.speed.y
-        }
 
         // object collision
         let bottomBall = ball.pos.y + ball.size;
-        this.topBall = his.game.ball.pos.y - ball.pos.size;
+        let topBall = ball.pos.y;
         let topObject = gameObject.pos.y;
         let leftObject = gameObject.pos.x;
         let rightObject = gameObject.pos.x + gameObject.width;
-        let bottomObject = gameObject.pos.x - gameObject.height;
+        let bottomObject = gameObject.pos.y + gameObject.height;
 
         if (
             bottomBall >= topObject &&
