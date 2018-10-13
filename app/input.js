f91f427ebacd06843217fe6d23e9ed225978583b
William George Cook <wgeorgecook@gmail.com>
2018-10-12 18:21:18 -0700

    Passing stop when keyup

--- app/input.js
+++ app/input.js
@@ -5,6 +5,12 @@ export default class InputHandler {
     constructor(paddle) {
         document.addEventListener('keydown', (e) => {
             paddle.move(e.keyCode)
+        });
+
+        document.addEventListener('keyup', (e) => {
+            if (e.keyCode === 37 || 39 ) {
+                paddle.stop(e.keyCode);
+            }
         })
     }
 }
